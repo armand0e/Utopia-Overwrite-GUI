8eddc1459236877161e5c3a742f7bb273e8c95eb
armand0e <53029190+armand0e@users.noreply.github.com>
2024-02-13 04:00:01 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 class Config:
-    maps_folder_path = "C:\\UDK"
-    # change this to the location of your maps folder (replace all \ with \\)
+    maps_folder_path = "C:\\UDK\\udk2015\\UDKGame\\Content\\Maps"
+    # change this to the location of your maps folder
+    # REPLACE ALL BACKSLASHES WITH A DOUBLE BACKSLASH!!! Program will not run otherwise. 
     
     epic_path = "C:\Program Files\Epic Games"
     steam_path = "C:\Program Files (x86)\Steam\steamapps\common"
